eee55d5b1dc170a3ec6021ae8a713c5c10190f71
ItsMrDav <davutt.simsek@gmail.com>
2022-11-15 18:49:11 +0100

    gotta work on it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import contactsJSON from "./contacts.json";
 
+const firstFive = contactsJSON.slice(0, 5);
 function App() {
+  const [contacts, setContacts] = useState(firstFive);
+  console.log(firstFive);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <button>Add Random Contacts</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((oneCeleb) => {
+            return (
+              <tr key={oneCeleb.id}>
+                <td>
+                  <img src={oneCeleb.pictureUrl} />
+                </td>
+                <td>{oneCeleb.name}</td>
+                <td>{oneCeleb.popularity}</td>
+                <td>{oneCeleb.wonOscar === true ? <span>🏆</span> : ``}</td>
+                <td>{oneCeleb.wonEmmy === true ? <span>🏆</span> : ``}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
